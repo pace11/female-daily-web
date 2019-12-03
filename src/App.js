9a71a4c1fa07cceddc91bd17064e04e89c4283f0
pace11 <ryanjoker87@gmail.com>
2019-12-04 01:54:40 +0700

    chore: added .prettierrc and svg asset

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import FemaleDaily from './assets/icons/FemaleDaily'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <FemaleDaily />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
