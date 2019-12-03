ed62aabb02cab29c4ce45d6b3ebba5e29d98babe
pace11 <ryanjoker87@gmail.com>
2019-12-04 02:52:45 +0700

    feat: added redux with easy-peasy

---
+++ src/redux/models/home/index.js
@@ -0,0 +1,37 @@
+import { action, thunk } from 'easy-peasy'
+import Axios from 'axios'
+
+const Home = {
+  initialState: {
+    loading: true,
+    error: false,
+    errorMessage: null,
+    data: null,
+  },
+
+  fetchAction: action((state, payload) => {
+    state.initialState.items = payload
+    state.initialState.loading = false
+  }),
+
+  error: action((state, payload) => {
+    state.initialState.loading = true
+    state.initialState.error = true
+    state.initialState.errorMessage = payload
+  }),
+
+  getHome: thunk(async (actions, payload) => {
+    try {
+      const config = {
+        method: 'get',
+        url: 'https://virtserver.swaggerhub.com/hqms/FDN-WP/0.1/wp',
+      }
+      let response = await Axios(config)
+      actions.fetchAction(response.data)
+    } catch (error) {
+      actions.error(error || error.errorMessage)
+    }
+  }),
+}
+
+export default Home
