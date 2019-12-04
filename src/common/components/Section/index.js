411791d38ee07c4333776b5d5a5d9a4feb41e116
pace11 <ryanjoker87@gmail.com>
2019-12-04 20:17:02 +0700

    feat: added component section

---
+++ src/common/components/Section/index.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { ArrowForwardIos } from '@material-ui/icons'
+import { Link } from 'react-router-dom'
+import theme from '../../theme'
+
+const StyledSection = styled.section`
+  padding: ${props => props.padding};
+  margin: ${props => props.margin};
+  background-color: ${props => props.backgroundColor};
+`
+
+const StyledBody = styled.div`
+  ${props =>
+    props.center &&
+    css`
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    `}
+`
+
+const StyledPanel = styled.div`
+  display: flex;
+  justify-content: space-between;
+  flex-direction: row;
+`
+
+const StyledTitle = styled.h3`
+  color: ${theme.colors.black_75};
+`
+
+const Title = styled.p`
+  font-size: 22px;
+  color: ${theme.colors.black};
+`
+
+const Subtitle = styled(Title)`
+  margin-top: -15px;
+  font-size: 20px;
+  color: ${theme.colors.gray};
+`
+
+const StyledLink = styled(Link)`
+  position: relative;
+  width: 100px;
+  height: auto;
+  margin-top: 35px;
+  text-decoration: none;
+  color: ${theme.colors.red};
+  div {
+    position: absolute;
+  }
+`
+
+const LinkTitle = styled.div`
+  width: auto;
+  font-size: 18px;
+`
+
+const WrapperIcon = styled.div`
+  width: auto;
+  margin-left: 80px;
+`
+
+function Section({
+  padding,
+  margin,
+  backgroundColor,
+  borderRadius,
+  title,
+  subtitle,
+  linkTo,
+  children,
+  center,
+}) {
+  return (
+    <StyledSection
+      padding={padding}
+      margin={margin}
+      backgroundColor={backgroundColor}
+      borderRadius={borderRadius}
+    >
+      <StyledPanel>
+        <StyledTitle>
+          {title && title.length > 0 && <Title>{title}</Title>}
+          {subtitle && subtitle.length > 0 && (
+            <Subtitle>{subtitle}</Subtitle>
+          )}
+        </StyledTitle>
+        {linkTo && linkTo.length > 0 && (
+          <StyledLink to={linkTo}>
+            <LinkTitle>See More</LinkTitle>
+            <WrapperIcon>
+              <ArrowForwardIos />
+            </WrapperIcon>
+          </StyledLink>
+        )}
+      </StyledPanel>
+      <StyledBody center={center}>{children}</StyledBody>
+    </StyledSection>
+  )
+}
+
+Section.defaultProps = {
+  padding: '0.625rem 0',
+  margin: '0px 0px 0px 0px',
+  backgroundColor: theme.colors.white,
+  title: '',
+  subtitle: '',
+  linkTo: '',
+  center: false,
+}
+
+export default Section
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 import Header from '../../common/components/Header'
+import Section from '../../common/components/Section'
+
+const Container = styled.div`
+  margin-top: 100px;
+`
 
 export default function Home() {
   return (
     <React.Fragment>
       <Header />
-      <p>Aku Bisa</p>
+      <Container>
+        <Section
+          padding="1rem 1rem"
+          title="Choice Now"
+          subtitle="Hehehe"
+          linkTo="/detail"
+          center
+        >
+          Ini isinya
+        </Section>
+      </Container>
     </React.Fragment>
   )
 }
